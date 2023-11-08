b9c71672ec8d5b4204e16071559b0943db4fc648
MHFERNANDO <fermarhug@gmail.com>
2023-11-08 11:41:39 -0500

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Service learning',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
